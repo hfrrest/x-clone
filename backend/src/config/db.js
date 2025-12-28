a8e336e2f0379a1161dd6a7fc5663166aa538f2c
hfrrest <hfrrest@users.noreply.github.com>
2025-12-28 08:25:11 -0500

    backend done

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,14 +1,12 @@
-import mongoose from 'mongoose';
-import { ENV } from '../config/env.js';
+import mongoose from "mongoose";
+import { ENV } from "./env.js";
 
 export const connectDB = async () => {
-    try {
-        await mongoose.connect(ENV.MONGO_URI)
-        console.log('Connected to MONGODB SUCCESSFULLY')
-
-    } catch (error) {
-        console.log('Error connecting to MONGODB')
-        process.exit(1);
-
-    }
+  try {
+    await mongoose.connect(ENV.MONGO_URI);
+    console.log("Connected to DB SUCCESSFULLY ✅");
+  } catch (error) {
+    console.log("Error connecting to MONGODB");
+    process.exit(1);
+  }
 };
